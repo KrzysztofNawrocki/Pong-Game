ab9d95105488eb6ecd3a4aee836cb954a033ecac
KrzysztofNawrocki <47433542+KrzysztofNawrocki@users.noreply.github.com>
2019-03-07 23:01:52 +0100

    Testing PyCharm git connection.

--- main_game.py
+++ main_game.py
@@ -10,32 +10,32 @@ pygame.display.set_caption('Pong-game')             #name of the app
 
 class pong_ball():
 
-    def __init__(self, window, colourRGB, initial_x, initial_y, radius, velocity):
+    def __init__(self, window, colourRGB, x, y, radius, velocity):
         self.window = window
         self.colourRGB = colourRGB
-        self.initial_x = initial_x
-        self.initial_y = initial_y
+        self.x = x
+        self.y = y
         self.radius = radius
         self.velocity = velocity
 
     def draw(self): #drawing a ball in the window
-        pygame.draw.circle(self.window, self.colourRGB, (self.initial_x, self.initial_y), self.radius)
+        pygame.draw.circle(self.window, self.colourRGB, (self.x, self.y), self.radius)
 
 class paddle():
-    def __init__(self, window, colourRGB, width, height, initial_x, initial_y, velocity):
+    def __init__(self, window, colourRGB, width, height, x, y, velocity):
         self.window = window
         self.colourRGB = colourRGB
         self.width = width
         self.height = height
-        self.initial_x = initial_x
-        self.initial_y = initial_y
+        self.x = x
+        self.y = y
         self.velocity = velocity
 
     def draw(self):
-        pygame.draw.rect(self.window, self.colourRGB, (self.initial_x - self.width / 2, self.initial_y, self.width, self.height))
+        pygame.draw.rect(self.window, self.colourRGB, (self.x - self.width / 2, self.y, self.width, self.height))
 
 
-new_ball = pong_ball(window, (255, 0, 100), int(width / 2), int(height / 2), 10, 5) #new pong ball
+new_ball = pong_ball(window, (255, 0, 100), int(width / 2), int(height / 2), 10, 15) #new pong ball
 player_1 = paddle(window, (0, 100, 100), width / 8, height / 50, int(width / 2), 0 + int(height / 20), width/30)
 player_2 = paddle(window, (0, 100, 100), width / 8, height / 50, int(width / 2), int(height) - int(height / 20), width/30)
 
@@ -54,18 +54,23 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-
-
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
-        player_1.initial_x -= player_1.velocity
+        player_1.x -= player_1.velocity
     if keys[pygame.K_RIGHT]:
-        player_1.initial_x += player_1.velocity
+        player_1.x += player_1.velocity
     if keys[pygame.K_a]:
-        player_2.initial_x -= player_2.velocity
+        player_2.x -= player_2.velocity
     if keys[pygame.K_d]:
-        player_2.initial_x += player_2.velocity
+        player_2.x += player_2.velocity
+
+    ball_direction = 1
+    if new_ball.y <= player_1.y or new_ball.y >= player_2.y:
+        ball_direction *= -1
+    else:
+        pass
+    new_ball.y += ball_direction * new_ball.velocity
 
     game_window_redraw()
 
